0bfb51d700031098a2a7a4ac6d414129c28f171a
luhobrecht <luhobrecht@hotmail.com>
2023-06-22 14:12:04 +0200

    created repository and add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppToolbarComponent } from './app-toolbar/app-toolbar.component';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import { AppMainComponent } from './app-main/app-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppToolbarComponent,
+    AppNavbarComponent,
+    AppMainComponent
   ],
   imports: [
     BrowserModule
